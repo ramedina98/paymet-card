cc0504fb65274e066f4ae3871bfff14c7654fb46
ramedina98 <b_stone99@outlook.es>
2023-07-04 11:05:32 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  //hook to chences from display = none to displa = block...
+  const [display, setDisplay] = useState('none');
+  //we check the state of the display to send the 
+  //necesari comand...
+  const handleClick = () => {
+    if(display === 'none'){
+      setDisplay('block')
+    }else{
+      setDisplay('none')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='cont_all'>
+          <div className='logo'>
+            <h2>Jal Bank</h2>
+          </div>
+          <nav>
+            <ul>
+              <a href='#'><li>Contact Us</li></a>
+              <a href='#'><li>Other Services</li></a>
+              <a href='#'><li>Legal Terms</li></a>
+              <li className='mode' onClick={handleClick}>
+                Mode
+              </li>
+              {/*This will bie appear when the user
+              click the item MODE...*/}
+              <ul className='mode_options' style={ {display} }>
+                <li className='item'>Dark</li>
+                <li className='item'>Ligth</li>
+              </ul>
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
   );
