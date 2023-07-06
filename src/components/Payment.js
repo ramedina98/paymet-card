6beae22eb50be276320fb3f793fa7fefe5d819d4
ramedina98 <b_stone99@outlook.es>
2023-07-06 12:11:08 -0600

    se avanzo con el footer y con la toma de informacion de los inputs

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,8 +1,47 @@
 //here we have all the payment method...
 import React from 'react'
 import '../App.css';
+import { useState, useEffect } from 'react';
+
+//Aqui tenemos que continuar con las validaciones de 
+//los inputs...
 
 function Payment() {
+    const [confirmed, setConfimed] = useState(false);
+    const [name, setName] = useState("jane appleseed");
+    const [carNumber, setCardNumber] = useState("0000 0000 0000 0000");  
+    const [month, setMonth] = useState("00"); 
+    const [year, setYear] = useState("00"); 
+    const [cvc, setCvc] = useState("000");
+    //para checar si hay error o no y pintar de rojo...
+    const [hasError, setHasError] = useState(false);
+
+    //Aqui comprobamos que solo se manden letras no numeros...
+    const handleNameChange = (event) => {
+        const inputValue = event.target.value;
+        const filteredValue = inputValue.replace(/[^a-zA-Z ]/g, '');
+        setName(filteredValue);
+
+        // Verificar si hay caracteres no permitidos
+        if (inputValue !== filteredValue) {
+            setHasError(true);
+        } else {
+            setHasError(false);
+        }
+    };
+
+    //Funcion para el card number...
+    const handleCardNumberChange = (event) => {
+        const inputCardNumber = event.target.value;
+        const cleanedCardNumber = inputCardNumber.replace(/\D/g, '');
+        const formattedCardNumber = cleanedCardNumber
+            .replace(/(.{4})/g, '$1 ')
+            .trim()
+            .slice(0, 19);
+        const truncatedCardNumber = formattedCardNumber.slice(0, 19);
+        setCardNumber(truncatedCardNumber);
+    };
+
     return (
         <div className='main'>
             <div className='cont_img'></div>
@@ -19,14 +58,14 @@ function Payment() {
 
                             <div className='numbersNname'>
                                 <div className='numbers'>
-                                    <h2>0000 0000 0000 0000</h2>
+                                    <h2>{carNumber}</h2>
                                 </div>
                                 <div className='name'>
                                     <div className='n'>
-                                        <h4>jane appleseed</h4>
+                                        <h4>{name}</h4>
                                     </div>
                                     <div className='date'>
-                                        <h4>00/00</h4>
+                                        <h4>{month}/{year}</h4>
                                     </div>
                                 </div>
                             </div>
@@ -36,7 +75,7 @@ function Payment() {
                     <div className='back_card_cont'>
                         <div className='back_card'>
                             <div className='cont_cuv'>
-                                <p>000</p>
+                                <p>{cvc}</p>
                             </div>
                         </div>
                     </div>
@@ -46,23 +85,60 @@ function Payment() {
                     <form>
                         <div className='cont_input_name'>
                             <label for='name'>carholder name</label>
-                            <input type='text' name='name' placeholder='e.g.Jane Applessed'/>
+                            <input 
+                                type='text' 
+                                name='name' 
+                                placeholder='e.g.Jane Applessed'
+                                required
+                                maxLength={23}
+                                onChange={handleNameChange}
+                                className={hasError ? 'error' : ''}
+                            />
                         </div>
 
                         <div className='cont_input_number'>
                             <label for='number'>card number</label>
-                            <input type='text' name='number' placeholder='e.g.1234 5678 9123 0000'/>
+                            <input 
+                                type='text' 
+                                name='number' 
+                                
+                                placeholder='e.g.1234 5678 9123 0000'
+                                required
+                                onChange={handleCardNumberChange}
+                                maxLength={19}
+                            />
                         </div>
 
                         <div className='cont_input_date'>
                             <div className='date'>
                                 <label for='date'>exp. date(mm/yy)</label>
-                                <input type='text' name='date' placeholder='mm'/>
-                                <input type='text' name='date' placeholder='yy'/>
+                                <input 
+                                    type='text' 
+                                    name='date' 
+                                    placeholder='mm'
+                                    maxLength={2}
+                                    required
+                                    onChange={(e) => setMonth(e.target.value)}
+                                />
+                                <input 
+                                    type='text' 
+                                    name='date' 
+                                    placeholder='yy'
+                                    maxLength={2}
+                                    required
+                                    onChange={(e) => setYear(e.target.value)}
+                                />
                             </div>
                             <div className='cvc'>
                                 <label for='cvc'>cvc</label>
-                                <input type='text' name='cvc' placeholder='e.g.123'/>
+                                <input 
+                                    type='text' 
+                                    name='cvc' 
+                                    placeholder='e.g.123'
+                                    maxLength={3}
+                                    required
+                                    onChange={(e) => setCvc(e.target.value)}
+                                />
                             </div>
                         </div>
 
