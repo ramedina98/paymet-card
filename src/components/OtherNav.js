bae22d61bcd29d128f707143e5e548c141c1f6af
ramedina98 <b_stone99@outlook.es>
2023-07-06 09:17:49 -0600

    se implemento la api

--- src/components/OtherNav.js
+++ src/components/OtherNav.js
@@ -1,27 +1,45 @@
 /*Here we have the component that take the info from 
 the api and show them...*/
 import '../App.css';
-/*
-link para la api...
-https://ramedina98.github.io/api_nat/dataBankLinks.json*/
+import axios from "axios";
+import { useState, useEffect } from 'react';
 
 import React from 'react'
 
 function OtherNav() {
+
+    const [data, setData] = useState([]);
+    const peticionGet = async() => {
+        await axios.get("https://ramedina98.github.io/api_nat/dataBankLinks.json")
+        .then(response => {
+            console.log(response.data)
+            setData(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+        peticionGet();
+    },[]); 
+    
     return (
-        <div className='contenedor'>
-            <div className='title'>
-                <h2>Titulo</h2>
-            </div>
-            <div className='info'>
-                <ul>
-                    <li>Item</li>
-                    <li>Item</li>
-                    <li>Item</li>
-                    <li>Item</li>
-                </ul>
-            </div>
-        </div>
+        <>
+            {data && data.map(item => (
+                <div className='contenedor' key={item.id}>
+                    <div className='title'>
+                        <h2>{item.name}</h2>
+                    </div>
+                    <div className='info'>
+                        <ul>
+                            {item && item.items.map((lis) => (
+                            <li>{lis}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            ))}
+        </>
     )
 }
 
