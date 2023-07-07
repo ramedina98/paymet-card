187a734c22d5ed6ee72df53000fcfea4d9e1773b
ramedina98 <b_stone99@outlook.es>
2023-07-07 11:12:11 -0600

    termine con gran parte del proyecto, hay que checar detalles y hacer el responsive

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -7,27 +7,30 @@ import { useState, useEffect } from 'react';
 //los inputs...
 
 function Payment() {
-    const [confirmed, setConfimed] = useState(false);
+    const [confirmed, setConfirmed] = useState(false);
     const [name, setName] = useState("jane appleseed");
+    const [labelName, setLabelName] = useState("CARHOLDER NAME")
     const [carNumber, setCardNumber] = useState("0000 0000 0000 0000");  
+    const [labelNumbers, setLabelNumbers] = useState("CARD NUMBER");
     const [month, setMonth] = useState("00"); 
+    const [labelData, setLabelData] = useState("EXP. DATE(MM/YY)");
     const [year, setYear] = useState("00"); 
     const [cvc, setCvc] = useState("000");
+    const [labelCvc, setLabeCvc] = useState("CVC");
     //para checar si hay error o no y pintar de rojo...
     const [hasError, setHasError] = useState(false);
+    const [hasError1, setHasError1] = useState(false);
+    const [hasError2, setHasError2] = useState(false);
+    const [hasError3, setHasError3] = useState(false);
+    const [hasErro4, setHasError4] = useState(false); 
 
     //Aqui comprobamos que solo se manden letras no numeros...
     const handleNameChange = (event) => {
         const inputValue = event.target.value;
         const filteredValue = inputValue.replace(/[^a-zA-Z ]/g, '');
         setName(filteredValue);
-
-        // Verificar si hay caracteres no permitidos
-        if (inputValue !== filteredValue) {
-            setHasError(true);
-        } else {
-            setHasError(false);
-        }
+        setHasError(inputValue !== filteredValue);
+        setLabelName(inputValue !== filteredValue ? "No numbers" : "CARHOLDER NAME");
     };
 
     //Funcion para el card number...
@@ -40,7 +43,63 @@ function Payment() {
             .slice(0, 19);
         const truncatedCardNumber = formattedCardNumber.slice(0, 19);
         setCardNumber(truncatedCardNumber);
+        setHasError1(inputCardNumber !== cleanedCardNumber);
+        setLabelNumbers(inputCardNumber !== cleanedCardNumber ? "NO LTTERS" : "CARD NUMBER");
     };
+
+    //funcion para validar los inputs de fecha y cvc
+    const handleMonth = (e) => {
+        const inputValue = e.target.value;
+        const validar = inputValue.replace(/\D/g, '');
+        setMonth(validar);
+        setHasError2(inputValue !== validar);
+        setLabelData(inputValue !== validar ? "totally invalid format" : "EXP. DATE(MM/YY)");
+    }
+    const handleYear= (e) => {
+        const inputValue = e.target.value;
+        const validar = inputValue.replace(/\D/g, '');
+        setYear(validar);
+        setHasError3(inputValue !== validar);
+        setLabelData(inputValue !== validar ? "totally invalid format" : "EXP. DATE(MM/YY)");
+    }
+
+    //CVC function...
+    const handleCvc = (e) => {
+        const inputValue = e.target.value; 
+        const validar = inputValue.replace(/\D/g, '');
+        setCvc(validar); 
+        setHasError4(inputValue !== validar);
+        setLabeCvc(inputValue !== validar ? "totally invalid format" : "CVC");
+    }
+
+    /*En estas funciones validamos que ningun campo
+    del form este vacio para poder hacer sumit*/
+    // Verificar si todos los campos están completos
+    const isFormValid = () => {
+        return name && carNumber && month && year && cvc;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Validar si todos los campos están completos
+        if (isFormValid()) {
+            setConfirmed(true);
+            console.log('Formulario confirmado');
+        } else {
+            alert("Exito");
+        }
+    };
+
+    //para retornar al formulario anterior...
+    const handleContinue = (e) => {
+        setName('jane appleseed');
+        setCardNumber('0000 0000 0000 0000');
+        setCvc('000');
+        setYear('00'); 
+        setMonth('00');
+        setConfirmed(false);
+    }
 
     return (
         <div className='main'>
@@ -82,9 +141,21 @@ function Payment() {
                 </div>
 
                 <div className='inputs_cont'>
-                    <form>
+                    {confirmed ? (
+                        <div className='father_cont_thanks'>
+                            <div className='cont_thanks'>
+                                <div className='img'></div>
+                                <div className='cont_txt_thanks'>
+                                    <h2>thank you!</h2>
+                                    <p>We've added your card details</p>
+                                </div>
+                                <button onClick={handleContinue}>Continue</button>
+                            </div>
+                        </div>
+                    ) : (
+                        <form onSubmit={handleSubmit}>
                         <div className='cont_input_name'>
-                            <label for='name'>carholder name</label>
+                            <label for='name'>{labelName}</label>
                             <input 
                                 type='text' 
                                 name='name' 
@@ -97,7 +168,7 @@ function Payment() {
                         </div>
 
                         <div className='cont_input_number'>
-                            <label for='number'>card number</label>
+                            <label for='number'>{labelNumbers}</label>
                             <input 
                                 type='text' 
                                 name='number' 
@@ -105,20 +176,22 @@ function Payment() {
                                 placeholder='e.g.1234 5678 9123 0000'
                                 required
                                 onChange={handleCardNumberChange}
-                                maxLength={19}
+                                maxLength={16}
+                                className={hasError1 ? 'error' : ''}
                             />
                         </div>
 
                         <div className='cont_input_date'>
                             <div className='date'>
-                                <label for='date'>exp. date(mm/yy)</label>
+                                <label for='date'>{labelData}</label>
                                 <input 
                                     type='text' 
                                     name='date' 
                                     placeholder='mm'
                                     maxLength={2}
                                     required
-                                    onChange={(e) => setMonth(e.target.value)}
+                                    onChange={handleMonth}
+                                    className={hasError2 ? 'error' : ''}
                                 />
                                 <input 
                                     type='text' 
@@ -126,7 +199,8 @@ function Payment() {
                                     placeholder='yy'
                                     maxLength={2}
                                     required
-                                    onChange={(e) => setYear(e.target.value)}
+                                    onChange={handleYear}
+                                    className={hasError3 ? 'error' : ''}
                                 />
                             </div>
                             <div className='cvc'>
@@ -137,13 +211,15 @@ function Payment() {
                                     placeholder='e.g.123'
                                     maxLength={3}
                                     required
-                                    onChange={(e) => setCvc(e.target.value)}
+                                    onChange={handleCvc}
+                                    className={hasErro4 ? 'error' : ''}
                                 />
                             </div>
                         </div>
 
                         <button className='send' type='submit'>confirm</button>
                     </form>
+                    )}
                 </div>
             </div>
         </div>
